4755ee417474916b0ef3e8cc077f8ef26ec50d6e
nutboltu <farhad.yasir.nb@gmail.com>
2021-11-30 20:11:08 +1100

    test: fixing unit tests

--- src/utils/faker.test.js
+++ src/utils/faker.test.js
@@ -1,47 +1,6 @@
 import { Faker } from './faker';
 
 describe('Faker', () => {
-    describe('getNormalizedUrl', () => {
-        const faker = new Faker();
-
-        it('should remove protocol like http', () => {
-            const input = 'http://google.com/test';
-            const actual = faker.getNormalizedUrl(input);
-            expect(actual.path).toEqual('google.com/test');
-            expect(actual.searchParamKeys).toEqual([]);
-        });
-        it('should return relative path', () => {
-            const input = '/test/foo/bar';
-            const actual = faker.getNormalizedUrl(input);
-            expect(actual.path).toEqual('localhost/test/foo/bar');
-            expect(actual.searchParamKeys).toEqual([]);
-        });
-        it('should return relative path without slash infront', () => {
-            const input = 'test/foo/bar';
-            const actual = faker.getNormalizedUrl(input);
-            expect(actual.path).toEqual('localhost/test/foo/bar');
-            expect(actual.searchParamKeys).toEqual([]);
-        });
-        it('should remove protocol like https', () => {
-            const input = 'https://google.com/test';
-            const actual = faker.getNormalizedUrl(input);
-            expect(actual.path).toEqual('google.com/test');
-            expect(actual.searchParamKeys).toEqual([]);
-        });
-
-        it('should keep port with the hostname', () => {
-            const input = 'https://google.com:8080/test';
-            const actual = faker.getNormalizedUrl(input);
-            expect(actual.path).toEqual('google.com:8080/test');
-            expect(actual.searchParamKeys).toEqual([]);
-        });
-        it('should return query params key with the hostname', () => {
-            const input = 'https://google.com:8080/test?all=true&only=false';
-            const actual = faker.getNormalizedUrl(input);
-            expect(actual.path).toEqual('google.com:8080/test');
-            expect(actual.searchParamKeys).toEqual(['all', 'only']);
-        });
-    });
     describe('getKey', () => {
         const faker = new Faker();
 
