eb7df1cb09045006203828fe0d068f8a353505c3
nutboltu <farhad.yasir.nb@gmail.com>
2023-04-09 13:43:20 +1000

    chore: pipeline support node 18 and 16

--- packages/mock-addon-docs/stories/examples/components/container/styles.js
+++ packages/mock-addon-docs/stories/examples/components/container/styles.js
@@ -1,6 +1,5 @@
 export const containerStyles = {
     color: 'CanvasText',
-    colorScheme: 'light dark',
     display: 'table',
     fontFamily: `"Nunito Sans",-apple-system,".SFNSText-Regular","San Francisco",BlinkMacSystemFont,"Segoe UI","Helvetica Neue",Helvetica,Arial,sans-serif`,
     padding: '0 1rem',
