ca27e5c67c4f9baf91e5179d609e530e3136d6f4
katarzynaB99 <50154454+katarzynaB99@users.noreply.github.com>
2023-08-23 16:33:02 +0000

    Use the Headers constructor to create response headers

--- packages/mock-addon/src/utils/response.js
+++ packages/mock-addon/src/utils/response.js
@@ -4,9 +4,9 @@ import { defaultResponseHeaders } from './headers';
 export function Response(url, status, responseText) {
     const keys = [];
     const all = [];
-    const headers = {
+    this.headers = new Headers({
         ...defaultResponseHeaders,
-    };
+    });
     // eslint-disable-next-line no-bitwise
     this.ok = ((status / 100) | 0) === 2; // 200-299
     this.statusText = statusTextMap[status.toString()];
@@ -20,11 +20,11 @@ export function Response(url, status, responseText) {
                 : JSON.stringify(responseText)
         );
     this.json = () => Promise.resolve(responseText);
-    (this.clone = () => new Response(url, status, responseText)),
-        (this.headers = {
-            keys: () => keys,
-            entries: () => all,
-            get: (n) => headers[n.toLowerCase()],
-            has: (n) => n.toLowerCase() in headers,
-        });
+    (this.clone = () => new Response(url, status, responseText))
+        // (this.headers = {
+        //     keys: () => keys,
+        //     entries: () => all,
+        //     get: (n) => headers[n.toLowerCase()],
+        //     has: (n) => n.toLowerCase() in headers,
+        // });
 }
